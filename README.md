ac972d28dd8bea9120d6b9baa20957ee3b57451b
Delvin N J <someytomy03@gmail.com>
2023-07-16 10:42:43 +0530

    Remove all files

--- README.md
+++
@@ -1,7 +0,0 @@
-# avodhaShopping
-This is a demo project. So this project is not working properly.
-In this project contain
-  . Registration 
-  . Login/Logout
-  . Add Product and category
-  . Add to cart 
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    # path('grapes',views.grapes,name='grapes'),
-    path('register/',views.Register,name='register'),
-    path('logout/',views.Logout,name='logout'),
-]
